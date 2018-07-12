ecebb376dae81c2742d9e87629f18273c931aee5
Kayler Cookhorn <kmcookhorn@gmail.com>
2018-07-12 17:32:01 -0400

    final with wireframe link added and function name change

--- main.js
+++ main.js
@@ -1,83 +1,83 @@
-document.getElementById('pic1').addEventListener('mouseover', mouseOver);
-document.getElementById('pic1').addEventListener('mouseout', mouseOut);
+document.getElementById('pic1').addEventListener('mouseover', hoverOver);
+document.getElementById('pic1').addEventListener('mouseout', hoverOut);
 // var text = document.getElementById('text1').value
 
-function mouseOver() {
+function hoverOver() {
   document.getElementById('pic1').style.opacity=0.5;
   document.getElementById('text1').style.opacity=1;
 }
-function mouseOut() {
+function hoverOut() {
   document.getElementById('pic1').style.opacity=1;
   document.getElementById('text1').style.opacity=0;
 
 }
 
  // +++++++++++++pic2+
- document.getElementById('pic2').addEventListener('mouseover', mouseOver2);
- document.getElementById('pic2').addEventListener('mouseout', mouseOut2);
+ document.getElementById('pic2').addEventListener('mouseover', hoverOver2);
+ document.getElementById('pic2').addEventListener('mouseout', hoverOut2);
 
- function mouseOver2() {
+ function hoverOver2() {
    document.getElementById('pic2').style.opacity=0.5;
    document.getElementById('text2').style.opacity=1;
  }
 
- function mouseOut2() {
+ function hoverOut2() {
    document.getElementById('pic2').style.opacity=1;
    document.getElementById('text2').style.opacity=0;
  }
 
 
  // +++++++++++++pic3+++
- document.getElementById('pic3').addEventListener('mouseover', mouseOver3);
- document.getElementById('pic3').addEventListener('mouseout', mouseOut3);
+ document.getElementById('pic3').addEventListener('mouseover', hoverOver3);
+ document.getElementById('pic3').addEventListener('mouseout', hoverOut3);
 
- function mouseOver3() {
+ function hoverOver3() {
   document.getElementById('pic3').style.opacity=0.5;
   document.getElementById('text3').style.opacity=1;
  }
 
- function mouseOut3() {
+ function hoverOut3() {
    document.getElementById('pic3').style.opacity=1;
    document.getElementById('text3').style.opacity=0;
    }
 
 
  // +++++++++++++pic4+++
- document.getElementById('pic4').addEventListener('mouseover', mouseOver4);
- document.getElementById('pic4').addEventListener('mouseout', mouseOut4);
+ document.getElementById('pic4').addEventListener('mouseover', hoverOver4);
+ document.getElementById('pic4').addEventListener('mouseout', hoverOut4);
 
- function mouseOver4() {
+ function hoverOver4() {
    document.getElementById('pic4').style.opacity=0.5;
    document.getElementById('text4').style.opacity=1;
  }
 
- function mouseOut4() {
+ function hoverOut4() {
    document.getElementById('pic4').style.opacity=1;
    document.getElementById('text4').style.opacity=0;
  }
  // ++++++++++++pic5++++
- document.getElementById('pic5').addEventListener('mouseover', mouseOver5);
- document.getElementById('pic5').addEventListener('mouseout', mouseOut5);
+ document.getElementById('pic5').addEventListener('mouseover', hoverOver5);
+ document.getElementById('pic5').addEventListener('mouseout', hoverOut5);
 
- function mouseOver5() {
+ function hoverOver5() {
    document.getElementById('pic5').style.opacity=0.5;
    document.getElementById('text5').style.opacity=1;
  }
 
- function mouseOut5() {
+ function hoverOut5() {
    document.getElementById('pic5').style.opacity=1;
    document.getElementById('text5').style.opacity=0;
  }
  // +++++++++++++pic6+++
- document.getElementById('pic6').addEventListener('mouseover', mouseOver6);
- document.getElementById('pic6').addEventListener('mouseout', mouseOut6);
+ document.getElementById('pic6').addEventListener('mouseover', hoverOver6);
+ document.getElementById('pic6').addEventListener('mouseout', hoverOut6);
 
- function mouseOver6() {
+ function hoverOver6() {
    document.getElementById('pic6').style.opacity=0.5;
    document.getElementById('text6').style.opacity=1;
  }
 
- function mouseOut6() {
+ function hoverOut6() {
    document.getElementById('pic6').style.opacity=1;
    document.getElementById('text6').style.opacity=0;
  }
